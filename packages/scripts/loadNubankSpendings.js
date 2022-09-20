2c0e4e31f147825e864990f0a116c7f758454ef2
Bruno Oliveira <brunohdbass@gmail.com>
2022-09-20 12:39:56 -0300

    loadNubankSpendings

--- packages/scripts/loadNubankSpendings.js
+++ packages/scripts/loadNubankSpendings.js
@@ -1,31 +1,26 @@
-import { pipe } from 'https://unpkg.com/@excelsia/pipe@1.0.0/dist/index.js'
+import { pipe, safe } from 'https://unpkg.com/@excelsia/general-helpers@latest/esm/index.js';
+import { prop, trim, replace } from 'https://unpkg.com/rambda@7.2.1/dist/rambda.mjs';
 import * as dom from 'https://unpkg.com/@excelsia/dom@1.0.0/dist/index.js'
 
 const main = async () => {
-  console.log(pipe, dom)
-  
-  // const lastColumnSpendings = () => pipe(
-  //   dom.last('.charges.column', document.body),
-  //   maybe.map(dom.all('.charge')),
-  //   maybe.map(list.map(element => ({
-  //     date: pipe(dom.one('.date', element), maybe.map(prop('textContent')), maybe.fold),
-  //     description: pipe(dom.one('.description', element), maybe.map(prop('textContent')), maybe.fold),
-  //     value: pipe(
-  //       dom.one('.amount', element),
-  //       tap('amount'),
-  //       maybe.map(prop('textContent')),
-  //       tap('textContent'),
-  //       maybe.map(replaceAll('.', '')),
-  //       maybe.map(replace(',', '.')),
-  //       maybe.map(Number),
-  //       maybe.fold,
-  //     ),
-  //   }))),
-  //   maybe.fold,
-  // );
+  const lastColumnSpendings = (table) => pipe(
+    table,
+    safe.map(dom.all('.charge')),
+    safe.map(list => list.map(element => ({
+      date: pipe(dom.first('.date', element), safe.map(prop('textContent'))),
+      description: pipe(dom.first('.description', element), safe.map(prop('textContent')), safe.map(trim)),
+      value: pipe(
+        dom.first('.amount', element),
+        safe.map(prop('textContent')),
+        safe.map(replace(/\./g, '')),
+        safe.map(replace(',', '.')),
+        safe.map(Number),
+      ),
+    }))),
+  );
   
   
-  // window.lastColumnSpendings = lastColumnSpendings;
+  window.lastColumnSpendings = lastColumnSpendings;
 };
 
 setTimeout(main, 2000);
